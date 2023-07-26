7face4218de17ddb8021cd45a1d1389aed139c52
CchristiNana <139203070+CchristiNana@users.noreply.github.com>
2023-07-26 18:13:47 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
-let express = require("express")
-let app = express()
-let port = process.env.port || 3000;
+var express = require("express")
+var app = express()
 
-app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + '/'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.render('index.html');
-});
-
-app.get('/addTwoNumbers', (req, res) => {
-    //let payLoad = {}
-    let statusCode = 200;
-    let sucessMessage = 'successful';
-    let number1 = req.query.number1; //1
-    let number2 = req.query.number2; //2
-    let data = parseInt(number1) + parseInt(number2);
-
-    res.json({ message: sucessMessage, code: statusCode, data: data })
-});
-
+var port = process.env.port || 3000;
 app.listen(port, () => {
     console.log("App listening to: " + port)
 })
-
