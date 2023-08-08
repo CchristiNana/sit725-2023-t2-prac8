543b13bdeb12a9159686deef317346e444bafa3a
CchristiNana <139203070+CchristiNana@users.noreply.github.com>
2023-08-08 14:13:13 +1000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,53 +1,54 @@
-const cardList = [
-    {
-        title: "Kitten 2",
-        image: "kitten2.jpg",
-        link: "About Kitten 2",
-        desciption: "Demo description about kitten 2"
-    },
-    {
-        title: "Kitten 3",
-        image: "kitten3.jpg",
-        link: "About Kitten 3",
-        desciption: "Demo description about kitten 3"
-    }
-]
-
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = `<div class="col s4 center-align">
-            <div class="card medium">
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator card-image img" src="${item.image}">
-                </div>
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span>
-                    <p><a href="#">${item.link}</a></p>
-                </div>
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
-                    <p class="card-text">${item.description}</p>
-                </div>
-            </div>
-        </div>`;
-        $("#card-section").append(itemToAppend);
+        let itemToAppend = '<div class="col s4 center-align">'+
+                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
+                '</div><div class="card-content">'+
+                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+                '<div class="card-reveal">'+
+                '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
+                '<p class="card-text">'+item.description+'</p>'+
+                '</div></div></div>';
+        $("#card-section").append(itemToAppend)
     });
-};
-
-const submitForm = () => {
-    let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
-    console.log("Form Data Submitted: ", formData);
 }
 
-$(document).ready(function () {
+const formSumitted = () => {
+    let formData = {};
+    formData.title = $('#title').val();
+    formData.path = $('#path').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.description = $('#description').val();
+
+    console.log(formData);
+    postCat(formData);
+}
+
+function postCat(cat) {
+    $.ajax({
+        url:'/api/cat',
+        type:'POST',
+        data:cat,
+        success: (result) => {
+            if (result.statusCode === 201) {
+                alert('cat posted');
+            }
+        }
+    });
+}
+
+function getAllCats() {
+    $.get('/api/cats',(result)=>{
+        if (result.statusCode === 200) {
+            addCards(result.data);
+        }
+    });
+}
+
+$(document).ready(function(){
     $('.materialboxed').materialbox();
-    $('#formSubmit').click(() => {
-        submitForm();
-    })
-    addCards(cardList);
+    $('#formSubmit').click(()=>{
+        formSumitted();
+    });
     $('.modal').modal();
+    getAllCats();
 });
--- server.js
+++ server.js
@@ -1,11 +1,63 @@
-var express = require("express")
-var app = express()
+let express = require('express');
+let app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.pwqw4ja.mongodb.net/?retryWrites=true&w=majority";
+let port = process.env.port || 3000;
+let collection;
 
-app.use(express.static(__dirname + '/'))
+app.use(express.static(__dirname + '/'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
-var port = process.env.port || 3000;
-app.listen(port, () => {
-    console.log("App listening to: " + port)
-})
+async function run() {
+    try {
+      await client.connect();
+      collection = client.db().collection('Cats');
+      console.log(collection);
+    } catch(ex) {
+      console.error(ex);
+    }
+}
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+});
+
+app.get('/', function (req,res) {
+    res.render('index.html');
+});
+
+app.post('/api/cat', function(req,res){
+    // ask DB to post this CAT
+    let cat = req.body;
+    insertCat(cat, (err,result) => {
+        if (!err) {
+            res.json({statusCode:201,data:result,message:'success'});
+        }
+    });
+});
+
+function insertCat(cat, callback) {
+    collection.insertOne(cat,callback);
+}
+
+app.get('/api/cats', (req,res)=>{
+    getAllCats((err,result)=>{
+        if (!err) {
+            res.json({statusCode:200,data:result,message:'success'});
+        }
+    });
+});
+
+function getAllCats(callback) {
+    collection.find({}).toArray(callback);
+}
+
+app.listen(port, ()=>{
+    console.log('express server started');
+    run();
+});
